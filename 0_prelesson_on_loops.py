463a7b073b67fc9cb681960972fbdb86f116ff6b
JesusT491 <jtorres217@cps.edu>
2025-12-09 21:00:47 +0000

    commit

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,30 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+
+# for loops = execute a block of code a fixed number of times.
+#               You can iterate over a range, string,sequence, etc
+#               Basically anything iterable
+
+#example
+
+# for i in (range(1,11,2)):
+# Start (inclusive), End (exclusive), Increment/Step
+#     print(i)
+
+# print("haPpy nEw yeAr ")
+
+
+# credit_card = '1234-1234-1234-1234'
+
+# for x in credit_card:
+#     print(x)
+
+for x in range(1,21):
+    if x == 13:
+        continue #skip over but continue
+    else:
+        print(x)
+
+for x in range(1,21):
+    if x == 10:
+        break #stops at 10
--- 1_loops.py
+++ 1_loops.py
@@ -1,7 +1,7 @@
 # Example Practice:
 # Given this list of fruits:
 fruits = ["apple", "banana", "cherry", "date"]
-
+print(len(fruits))
 # Challenge:
 # Use a for loop to print each fruit on a new line.
 print(fruits[0])
@@ -13,8 +13,27 @@ for fruit in fruits:
 
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
+print(len(subjects))
+
+#prints the list
 for subject in subjects:
     print(subject)
+
+#prints all but stops history
+for subject in subjects:
+    if subjects[2]:
+        break
+print(subject)
+
+#prints all but skips science
+for subject in subjects:
+    if subjects[1]:
+        continue
+print(subject)
+
+for index in range(len(subjects)):
+    print("Subject " + str(index) + ": " + subjects[index])
+
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
@@ -25,3 +44,33 @@ numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+
+
+total_sum = 0 # store the number in a variable (baseline)
+
+
+for num in numbers: #loop to iterate through every index in the list
+    total_sum += num #For the variable we created, adds up the numbers from each index and saves it to the total_sum
+
+print("total: ", total_sum)
+
+#challenge
+#sum all the numbers up and print the total
+new_numbers = list(range(1,600001))
+
+num1 = 0
+
+for num in new_numbers:
+    num1 += num
+print("total ", num1)
+
+#challenge3
+
+list = list(range(5,26))
+
+base_line = 0
+
+for num in list:
+    base_line += list
+
+print("total ", base_line)
